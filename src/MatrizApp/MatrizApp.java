7333e7b07290dfdde069e9092f347acdf19489b3
p1re <oriolnieto2006@gmail.com>
2024-12-21 11:03:02 +0100

    matriz-inicial

--- src/MatrizApp/MatrizApp.java
+++ src/MatrizApp/MatrizApp.java
@@ -1,5 +1,27 @@
 package MatrizApp;
 
-public class MatrizApp {
+import java.util.Random;
+import java.util.Scanner;
 
+public class MatrizApp {
+	public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        Random rand = new Random(); 
+        int matriz[][] = new int[5][5];
+        int rango = 101;
+        
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
+                matriz[i][j] = rand.nextInt(rango);
+            }
+        }
+        
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
+                System.out.print(matriz[i][j] + " ");
+            }
+            System.out.println();
+        }
+        
+}
 }
